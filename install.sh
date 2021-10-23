5a36b70f73c0cf645fb7058012dcd0cefb7ff200
Jordi Prats <jordi.prats@gmail.com>
2021-10-23 11:23:08 +0200

    add accesory files

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -f "./moneykeeper.py" ] && [ ! -f "./moneykeeper.desktop" ];
+then
+    echo "please run this file from the base directory"
+    exit 1
+fi
+
+if [ "$(id -u)" -eq 0 ];
+then
+    echo "please execute do not run as root"
+    exit 1
+fi
+
+sudo cp ./moneykeeper.py /usr/local/bin/moneykeeper
+
+if [ -d ~/.config/autostart ];
+then
+    cp ./moneykeeper.desktop ~/.config/autostart
+else
+    echo "skipping startup unable to locate ~/.config/autostart"
+fi
